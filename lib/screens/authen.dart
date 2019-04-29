b04d2d5effb7f430daea8a4739a634ef073869e4
jitti <scorpionjit@gmail.com>
2019-04-29 16:41:25 +0700

    Finish Day 1

--- lib/screens/authen.dart
+++ lib/screens/authen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter/widgets.dart';
 
 class Authen extends StatefulWidget {
   @override
@@ -6,17 +8,99 @@ class Authen extends StatefulWidget {
 }
 
 class _AuthenState extends State<Authen> {
+  Widget signUpButton() {
+    return RaisedButton(
+      color: Colors.blue[300],
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+      child: Text('Sign Up'),
+      onPressed: () {},
+    );
+  }
 
-Widget showAppName(){
-  return Text('Jitti Flutter');
-}
+  Widget signInButton() {
+    return RaisedButton(
+      color: Colors.green[200],
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+      child: Text('Sign In'),
+      onPressed: () {},
+    );
+  }
+
+  Widget emailTextFormField() {
+    return TextFormField(
+      decoration:
+          InputDecoration(labelText: 'Email :', hintText: 'you@mail.com'),
+    );
+  }
+
+  Widget passwordTextFormField() {
+    return TextFormField(
+      obscureText: true,
+      decoration: InputDecoration(
+          labelText: 'Password :', hintText: 'More 6 Charactor'),
+    );
+  }
+
+  Widget showAppName() {
+    return Text(
+      'JN Flutter',
+      style: TextStyle(
+          fontFamily: 'Acme',
+          fontSize: 25.0,
+          fontWeight: FontWeight.bold,
+          color: Colors.blue[800]),
+    );
+  }
+
+  Widget showLogo() {
+    return Image.asset('images/logo.png');
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(padding: EdgeInsets.only(top: 100.0),
+      resizeToAvoidBottomPadding: false,
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                colors: [Colors.greenAccent, Colors.white],
+                begin: Alignment(-1, -1))),
+        padding: EdgeInsets.only(top: 80.0),
         alignment: Alignment(0, -1),
-        child: showAppName(),
+        child: Column(
+          children: <Widget>[
+            Container(
+              width: 150.0,
+              height: 150.0,
+              child: showLogo(),
+            ),
+            Container(margin: EdgeInsets.only(top: 15.0), child: showAppName()),
+            Container(
+              margin: EdgeInsets.only(left: 50.0, right: 50.0),
+              child: emailTextFormField(),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 50.0, right: 50.0),
+              child: passwordTextFormField(),
+            ),
+            Container(
+              margin: EdgeInsets.only(left: 50.0, right: 50.0, top: 15.0),
+              child: Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Container(
+                      margin: EdgeInsets.only(right: 4.0),
+                      child: signInButton(),
+                    ),
+                  ),
+                  Expanded(
+                    child: signUpButton(),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
