ec62ac0b15462df57eca564a8e3333dbb211fd72
jitti <scorpionjit@gmail.com>
2019-04-30 13:53:46 +0700

    Validate Ok!

--- lib/screens/register.dart
+++ lib/screens/register.dart
@@ -9,6 +9,9 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+// Explicit
+  final formKey = GlobalKey<FormState>();
+
   Widget passwordTextField() {
     return TextFormField(
       obscureText: true,
@@ -16,9 +19,14 @@ class _RegisterState extends State<Register> {
           enabledBorder: OutlineInputBorder(
               borderRadius: BorderRadius.circular(10.0),
               borderSide: BorderSide(width: 1.0, color: Colors.blue)),
-          labelText: 'Password',
+          labelText: 'Password :',
           hintText: 'Type Your Password',
           icon: Icon(Icons.lock, color: Colors.red[200])),
+      validator: (String value) {
+        if (value.length < 6) {
+          return 'รหัสผ่านน้อยกว่า 6 ตัวอักษร';
+        }
+      },
     );
   }
 
@@ -31,6 +39,13 @@ class _RegisterState extends State<Register> {
           labelText: 'Email :',
           hintText: 'you@mail.com',
           icon: Icon(Icons.email, color: Colors.red[200])),
+      validator: (String value) {
+        if (value.length == 0) {
+          return 'กรุณากรอก Email Address!';
+        } else if (!((value.contains('@')) && (value.contains('.')))) {
+          return 'Email Address ไม่ถูกต้อง!';
+        }
+      },
     );
   }
 
@@ -46,6 +61,11 @@ class _RegisterState extends State<Register> {
             Icons.perm_identity,
             color: Colors.red[200],
           )),
+      validator: (String value) {
+        if (value.length == 0) {
+          return 'Please Fill Name!';
+        }
+      },
     );
   }
 
@@ -55,6 +75,7 @@ class _RegisterState extends State<Register> {
       tooltip: 'Upload To Firebase',
       onPressed: () {
         print('You click upload');
+        if (formKey.currentState.validate()) {}
       },
     );
   }
@@ -62,32 +83,35 @@ class _RegisterState extends State<Register> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.purple[400],
-        title: Text('Register'),
-        actions: <Widget>[uploadButton()],
-      ),
-      body: Container(
-        decoration: BoxDecoration(
-            gradient: RadialGradient(
-                colors: [Colors.white, Colors.blue[400]],
-                radius: 2.0,
-                center: Alignment(0, -1))),
-        padding: EdgeInsets.all(50.0),
-        child: Column(
-          children: <Widget>[
-            nameTextFormField(),
-            Container(
-              margin: EdgeInsets.only(
-                top: 15.0,
-                bottom: 15.0,
-              ),
-              child: emailTextFormField(),
-            ),
-            passwordTextField()
-          ],
+        resizeToAvoidBottomPadding: false,
+        appBar: AppBar(
+          backgroundColor: Colors.purple[400],
+          title: Text('Register'),
+          actions: <Widget>[uploadButton()],
         ),
-      ),
-    );
+        body: Form(
+          key: formKey,
+          child: Container(
+            decoration: BoxDecoration(
+                gradient: RadialGradient(
+                    colors: [Colors.white, Colors.blue[400]],
+                    radius: 2.0,
+                    center: Alignment(0, -1))),
+            padding: EdgeInsets.all(50.0),
+            child: Column(
+              children: <Widget>[
+                nameTextFormField(),
+                Container(
+                  margin: EdgeInsets.only(
+                    top: 15.0,
+                    bottom: 15.0,
+                  ),
+                  child: emailTextFormField(),
+                ),
+                passwordTextField()
+              ],
+            ),
+          ),
+        ));
   }
 }
