f6cd93946d6c586d923320d32a74cf9847817a40
jitti <scorpionjit@gmail.com>
2019-04-30 09:35:51 +0700

    Start 30 Apr

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import './screens/authen.dart';
 
 void main() {
@@ -8,6 +9,7 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp,DeviceOrientation.portraitDown]); // Lock moniter
     return MaterialApp(
       title: 'Jitti Firebase',
       home: Authen(),
